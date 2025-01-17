2af73003466b204e6d7c14a8fddec7d64d9c5b32
solesen1992 <150657753+SaraOlesen1992@users.noreply.github.com>
2025-01-17 10:18:13 +0100

    Added models to the API

--- API/API/Models/Appointment.cs
+++ API/API/Models/Appointment.cs
@@ -1,6 +1,22 @@
 ﻿namespace API.Models
 {
+    /* 
+     * Appointment class represents an appointment entity in the system.
+     * This class holds the details for a donor's appointment, including the 
+     * appointment's start time, end time, and a reference to the donor associated 
+     * with the appointment. 
+     * 
+     * Properties:
+     * - `AppointmentId`: A unique identifier for the appointment.
+     * `StartTime`: The date and time when the appointment starts.
+     * - `EndTime`: The date and time when the appointment ends.
+     * - `FK_donorId`: A foreign key reference to the donor associated with the appointment.
+     */
     public class Appointment
     {
+        // Properties
+        public int AppointmentId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int FK_donorId { get; set; }
     }
-}
--- API/API/Models/Donor.cs
+++ API/API/Models/Donor.cs
@@ -1,6 +1,24 @@
 ﻿namespace API.Models
 {
+    /// <summary>
+    /// Represents a donor in the system. 
+    /// This class holds the donor's personal information, including contact details, city, 
+    /// and blood type. It also contains a foreign key to the CityZipCode and BloodType entities 
+    /// that represent the donor's location and blood type.
+    /// </summary>
     public class Donor
     {
+        // Properties
+        public int? DonorId { get; set; } // Unique identifier for the donor (nullable)
+        public string CprNo { get; set; }
+        public string DonorFirstName { get; set; }
+        public string DonorLastName { get; set; }
+        public int DonorPhoneNo { get; set; }
+        public string DonorEmail { get; set; }
+        public string DonorStreet { get; set; }
+        public int FK_CityZipCodeId { get; set; }
+        public int? FK_BloodTypeId { get; set; } // Foreign key to the BloodType entity (nullable)
+        public BloodTypeEnum? BloodType { get; set; } // Enum representing the donor's blood type
+        public CityZipCode CityZipCode { get; set; }
     }
 }
