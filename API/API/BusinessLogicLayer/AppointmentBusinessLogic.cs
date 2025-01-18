1ae857f9c25cae169eebc5fdb859e8a0f0053666
solesen1992 <saramundal@live.dk>
2025-01-18 10:55:18 +0100

    Adding interface

--- API/API/BusinessLogicLayer/AppointmentBusinessLogic.cs
+++ API/API/BusinessLogicLayer/AppointmentBusinessLogic.cs
@@ -1,7 +1,37 @@
-﻿namespace API.BusinessLogicLayer
-{
-    public class AppointmentBusinessLogic : IAppointmentLogic
-    {
+﻿using API.Models;
 
+namespace API.BusinessLogicLayer
+{
+    public class AppointmentBusinessLogic : IAppointmentBusinessLogic
+    {
+        public bool DeleteAppointment(Appointment appointment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAppointmentByStartTime(DateTime startTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Appointment GetAppointmentById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Appointment> GetAppointments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Appointment> GetAppointmentsByDonorId(int donorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertAppointment(Appointment appointment, int donorId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
