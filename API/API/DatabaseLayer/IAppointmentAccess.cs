5c1e2a215404d80e7ed39130442df1ddcb8dfdea
solesen1992 <saramundal@live.dk>
2025-01-20 09:14:51 +0100

    Making interface

--- API/API/DatabaseLayer/IAppointmentAccess.cs
+++ API/API/DatabaseLayer/IAppointmentAccess.cs
@@ -1,6 +1,16 @@
 ﻿namespace API.DatabaseLayer
 {
+    /// <summary>
+    /// This interface defines the operations for managing appointment data within the database.
+    /// It includes methods for retrieving, inserting, updating, and deleting appointments.
+    /// </summary>
     public interface IAppointmentAccess
     {
+        public List<Appointment> GetAppointments();
+        public bool InsertAppointment(Appointment appointment, int donorId);
+        public bool DeleteAppointment(Appointment appointment);
+        Appointment GetAppointmentById(int id);
+        public List<Appointment> GetAppointmentsByDonorId(int donorId);
+        public bool DeleteAppointmentByStartTime(DateTime startTime);
     }
 }
