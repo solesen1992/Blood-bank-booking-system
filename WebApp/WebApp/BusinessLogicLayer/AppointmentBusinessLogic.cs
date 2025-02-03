5ff094060a52588a4002d2af9642fdf0ca2df516
solesen1992 <saramundal@live.dk>
2025-02-03 15:22:39 +0100

    Changes made

--- WebApp/WebApp/BusinessLogicLayer/AppointmentBusinessLogic.cs
+++ WebApp/WebApp/BusinessLogicLayer/AppointmentBusinessLogic.cs
@@ -1,5 +1,12 @@
 ﻿namespace WebApp.BusinessLogicLayer
 {
+    /// <summary>
+    /// Implements the <see cref="IAppointmentBusinessLogic"/> interface and handles the business logic related to appointments.
+    /// Responsible for validating appointment data, ensuring the integrity of appointment times, and interacting with the service layer 
+    /// to create appointments and retrieve unavailable appointment times.
+    /// Encapsulates the logic for validating time constraints (e.g., appointments cannot be in the past or too far in the future) 
+    /// and communicates with the underlying service layer to perform CRUD operations on appointments.
+    /// </summary>
     public class AppointmentBusinessLogic
     {
     }
--- WebApp/WebApp/BusinessLogicLayer/IAppointmentBusinessLogic.cs
+++ WebApp/WebApp/BusinessLogicLayer/IAppointmentBusinessLogic.cs
@@ -1,6 +1,16 @@
-﻿namespace WebApp.BusinessLogicLayer
+﻿using WebApp.Models;
+
+namespace WebApp.BusinessLogicLayer
 {
+    // Defines the contract for appointment-related business logic.
     public interface IAppointmentBusinessLogic
     {
+        public bool IsValidAppointmentDate(DateTime startTime);
+        public bool IsValidAppointmentTimes(DateTime startTime);
+        public bool CreateAppointment(Appointment appointment);
+        public List<string> GetUnavailableTimes(DateTime date);
+        public List<Appointment> GetFutureAppointments();
+        public List<Appointment> GetAppointmentsByDonorId(int donorId);
+        public bool DeleteAppointmentByStartTime(int donorId, DateTime startTime);
     }
 }
