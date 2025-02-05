7b00d444bbd74c53875080682acc977c3e0201c3
solesen1992 <saramundal@live.dk>
2025-02-05 15:06:29 +0100

    Changes made

--- WebApp/WebApp/ServiceLayer/AppointmentService.cs
+++ WebApp/WebApp/ServiceLayer/AppointmentService.cs
@@ -1,6 +1,19 @@
 ﻿namespace WebApp.ServiceLayer
 {
-    public class AppointmentService
+    /// <summary>
+    /// The AppointmentService class provides methods for interacting with the appointment-related endpoints 
+    /// of an external API. 
+    /// It acts as a service layer for handling appointment data and includes functionality for creating 
+    /// and retrieving appointments.
+    /// </summary>
+    /// <remarks>
+    /// - Uses the <see cref="HttpClient"/> class to make HTTP requests to the API.
+    /// - Relies on <see cref="JsonConvert"/> for serializing and deserializing data between JSON and C# objects.
+    /// - Retrieves the API base URL from configuration (e.g., appsettings.json) via dependency injection 
+    /// using the <see cref="IConfiguration"/> interface.
+    /// </remarks>
+    public class AppointmentServicee : IAppointmentService
     {
+
     }
 }
--- WebApp/WebApp/ServiceLayer/IAppointmentService.cs
+++ WebApp/WebApp/ServiceLayer/IAppointmentService.cs
@@ -1,6 +1,19 @@
-﻿namespace WebApp.ServiceLayer
+﻿using WebApp.Models;
+
+namespace WebApp.ServiceLayer
 {
+    /// <summary>
+    /// The IAppointmentService interface defines the contract for appointment-related operations.
+    /// It outlines the methods that must be implemented by any class handling appointment services.
+    /// </summary>
+    /// <remarks>
+    /// This interface ensures that the service layer provides functionality to create, retrieve, and delete appointments.
+    /// </remarks>
     public interface IAppointmentService
     {
+        public bool CreateAppointmentThroughApi(Appointment appointment);
+        public List<Appointment> GetExistingAppointments();
+        public List<Appointment> GetAppointmentsByDonorId(int donorId);
+        public bool DeleteAppointmentByStartTime(int donorId, DateTime startTime);
     }
 }
