87465974e8a4abd3e3bc9630718c7941ae232ef7
solesen1992 <saramundal@live.dk>
2025-02-11 11:28:08 +0100

    Comment changed

--- API/API/DTOs/CityZipCodeDTO.cs
+++ API/API/DTOs/CityZipCodeDTO.cs
@@ -1,17 +1,17 @@
 ﻿namespace API.DTOs
 {
-    /// <summary>
-    /// CityZipCodeDTO is a Data Transfer Object (DTO) used to represent a city and its corresponding zip code.
-    /// This class is designed to be used for communication between different layers.
-    ///
-    /// Properties:
-    /// - `City`: A string representing the name of the city.
-    /// - `ZipCode`: An integer representing the zip code associated with the city.
-    ///
-    /// The class provides two constructors:
-    /// - A parameterized constructor to initialize both the `City` and `ZipCode` properties.
-    /// - A parameterless constructor for scenarios where the values are set after instantiation.
-    /// </summary>
+    /**
+     * CityZipCodeDTO is a Data Transfer Object (DTO) used to represent a city and its corresponding zip code.
+     * This class is designed to be used for communication between different layers.
+     *
+     * Properties:
+     * - `City`: A string representing the name of the city.
+     * - `ZipCode`: An integer representing the zip code associated with the city.
+     *
+     * The class provides two constructors:
+     * - A parameterized constructor to initialize both the `City` and `ZipCode` properties.
+     * - A parameterless constructor for scenarios where the values are set after instantiation.
+     */
     public class CityZipCodeDTO
     {
         // Constructor
