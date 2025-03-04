7b23ca7ef01d42ad19c0137057fe93b981b992d9
solesen1992 <saramundal@live.dk>
2025-03-04 18:34:04 +0100

    Starting on donorDetails GUI

---
+++ DesktopApp/DesktopApp/GUI/DonorDetails.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DesktopApp.GUI
+{
+    partial class DonorDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            donorBox = new GroupBox();
+            cprLabel = new Label();
+            textBox1 = new TextBox();
+            donorBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // donorBox
+            // 
+            donorBox.Controls.Add(textBox1);
+            donorBox.Controls.Add(cprLabel);
+            donorBox.Location = new Point(25, 31);
+            donorBox.Name = "donorBox";
+            donorBox.Size = new Size(648, 435);
+            donorBox.TabIndex = 0;
+            donorBox.TabStop = false;
+            donorBox.Text = "Donor";
+            donorBox.Enter += groupBox1_Enter;
+            // 
+            // cprLabel
+            // 
+            cprLabel.AutoSize = true;
+            cprLabel.Location = new Point(19, 39);
+            cprLabel.Name = "cprLabel";
+            cprLabel.Size = new Size(71, 25);
+            cprLabel.TabIndex = 0;
+            cprLabel.Text = "CPR-nr.";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(105, 36);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(229, 31);
+            textBox1.TabIndex = 1;
+            // 
+            // DonorDetails
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(708, 804);
+            Controls.Add(donorBox);
+            Name = "DonorDetails";
+            Text = "DonorDetails";
+            donorBox.ResumeLayout(false);
+            donorBox.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox donorBox;
+        private TextBox textBox1;
+        private Label cprLabel;
+    }
+}
---
+++ DesktopApp/DesktopApp/GUI/DonorDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp.GUI
+{
+    public partial class DonorDetails : Form
+    {
+        public DonorDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
